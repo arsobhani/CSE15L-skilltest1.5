8fbaa1daa20ace4b336bd39b2a68683e2e4e5101
arsobhani <97648575+arsobhani@users.noreply.github.com>
2022-02-04 16:28:17 -0800

    Update skilltest2tester.java

--- skilltest2tester.java
+++ skilltest2tester.java
@@ -5,6 +5,6 @@ import org.junit.*;
 public class skilltest2tester {
     @Test
     public void multiply() {
-        assertEquals(5, skilltest2.multiply(2, 3));
+        assertEquals(6, skilltest2.multiply(2, 3));
     }
 }
